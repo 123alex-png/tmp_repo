ccda095aa4ca6ffdb20c477afc6b2a93950a0360
Qu Li <liqu@smail.nju.edu.cn>
2023-10-17 02:16:26 +0800

    coding is done.

--- utrace/attacher.cpp
+++ utrace/attacher.cpp
@@ -1,18 +1,56 @@
 #include "attacher.hh"
 #include <thread>
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <ctime>
+using std::string;
 
-static void gdb_run(attacher * self, int pid){
-    
+static void gdb_run(Stream * out, int pid,string breakPoint, string var){
+    int to_gdb[2],from_gdb[2];
+    if(pipe(to_gdb)<0||pipe(from_gdb)<0) assert(0);
+    pid_t pid=fork();
+    if(!pid){//run gdb
+        close(to_gdb[1]);close(from_gdb[0]);
+        dup2(to_gdb[0],STDIN_FILENO);
+        dup2(from_gdb[0],STDOUT_FILENO);
+        char Pid[40];
+        sprintf(Pid,"%d",pid);
+        char * args[]={
+            "gdb","-p",Pid
+        };
+        execv(args[0],args);
+    }else{//communicate with gdb
+        close(to_gdb[0]);close(from_gdb[1]);
+        static char buffer[4096];
+        int len=sprintf(buffer,"break %s\ncontinue\nprint %s",breakPoint.c_str(),var.c_str());
+        write(to_gdb[1],buffer,len);
+        while(1){
+            do{
+                read(from_gdb[0],buffer,1);
+            }while(buffer[0]!='$');
+            while(buffer[0]!='=') read(from_gdb[0],buffer,1);
+            int pos=0;
+            while(buffer[pos]!='\n') read(from_gdb[0],buffer+(++pos),1);
+            attacher::Val val;
+            val.first=time(NULL);
+            for(int i=1;i<pos;i++) val.second+=buffer[pos];
+            out->lock.lock();
+            out->q.push(val);
+            out->lock.unlock();
+            write(to_gdb[1],"continue\n",10);
+        }
+    }
 }
 
 void attacher::attach_watch(int pid,BreakPoint breakPoint,Var var){
-    std::thread t(gdb_run,this);
+    std::thread t(gdb_run,&this->stream,pid,breakPoint,var);
     return;
 }
 
 attacher::Val attacher::event(){
     stream.lock.lock();
-    Val ans="";
+    Val ans={0,""};
     if(!stream.q.empty()){
         ans=stream.q.front();
         stream.q.pop();
--- utrace/attacher.hh
+++ utrace/attacher.hh
@@ -4,27 +4,33 @@
 #include <queue>
 #include <string>
 #include <mutex>
+#include <utility>
+#include <ctime>
+
+//For simplicity, this watcher can only monitor ONE variable at ONE breakpoint for ONE process.
+//Variable ,breakpoint and var's value are all prased as string. The analysis of the string-form val is taken as user's work.
+//The watcher assumes that the user has root access and makes no mistake and the work never stop.
+//More variables and more breakpoints can temporily be done by more watcher, and this work is taken as one of the future's goal. 
+
+struct Stream;
 
 class attacher{
     private:
-        using Var=std::string;
-        using Val=std::string;
-        using BreakPoint=std::string;
         //for temporarily save the data
-        struct {
-            std::queue<Val> q;
-            std::mutex lock;
-        }stream;
-        //for saving the Vars to be watched
-        struct {
-            Var var;
-            BreakPoint breakPoint;
-        }controler;
+        Stream stream;
     public:
+        using Var=std::string;
+        using Val=std::pair<time_t,std::string>;
+        using BreakPoint=std::string;
         attacher()=default;
         //filename for dbg info
         void attach_watch(int pid,BreakPoint breakPoint,Var var);
         Val event();
+};
+
+struct Stream{
+    std::queue<attacher::Val> q;
+    std::mutex lock;
 };
 
 #endif
